2b5fa52984e943263971ee05b751f147ed86411f
kertys35 <kertys50@gmail.com>
2025-03-10 19:00:46 +0700

    Добавлен тест проверки победы в пятнашках.

--- Tests/Tests_Code.cpp
+++ Tests/Tests_Code.cpp
@@ -10,3 +10,9 @@ TEST_CASE("Create_Game_Object")
 	Game *game = new Game();
 	REQUIRE(game != NULL);
 }
+
+TEST_CASE("Check_win_condition")
+{
+	Game* game = new Game();
+	REQUIRE(game.Check(1) == 1);
+}
